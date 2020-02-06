ac22ac264c23854c976257a3e46693e18066e5f4
Simon J. Larsen <simonhffh@gmail.com>
2020-02-06 21:47:53 +0100

    Added better data format and support for other sequence types

--- README.md
+++ README.md
@@ -9,26 +9,44 @@ Simple Javascript sequence logo library using pure Javascript.
 <!DOCTYPE html>
 <html lang="en">
     <body>
-        <canvas id="logo_canvas"></canvas>
+        <h2>Nucleotides</h2>
+        <canvas id="logo_nt"></canvas>
+
+        <h2>Amino acids</h2>
+        <canvas id="logo_aa"></canvas>
 
         <script src="js/jseqlogo.js"></script>
         <script>
-            var columns = [
-                [["A", 0.5], ["C", 0.8],  ["G", 0.01], ["T", 0.2]],
-                [["A", 1.0], ["C", 0.05], ["G", 0.0],  ["T", 0.0]],
-                [["A", 0.0], ["C", 0.2],  ["G", 0.0],  ["T", 0.45]],
-                [["A", 0.1], ["C", 0.0],  ["G", 0.7],  ["T", 0.0]],
-                [["A", 0.0], ["C", 0.5],  ["G", 0.0],  ["T", 0.3]],
-                [["A", 0.0], ["C", 0.05], ["G", 0.3],  ["T", 0.0]],
-                [["A", 0.0], ["C", 0.0],  ["G", 0.84], ["T", 0.2]],
-            ];
-
-            var options = {
-                "ymax": 1
+            var data = {
+                "A": [0.5, 1.0, 0.0, 0.1, 0.0, 0.0, 0.0],
+                "C": [0.8, 0.05, 0.2, 0.0, 0.5, 0.05, 0.0],
+                "G": [0.01, 0.0, 0.0, 0.7, 0.0, 0.3, 0.84],
+                "T": [0.2, 0.0, 0.45, 0.0, 0.3, 0.0, 0.2]
             };
 
-            sequence_logo(document.getElementById("logo_canvas"), 600, 200, columns, options);
+            var options = {
+                "colors": jseqlogo.colors.nucleotides
+            };
+            sequence_logo(document.getElementById("logo_nt"), 600, 200, data, options);
+
+            var data = {
+                "A": [0.174, 0.0384, 0.111, 0.0311, 0.238, 0.0826, 0.191, 0.224],
+                "D": [0.231, 0.286, 0.422, 0.0904, 0.723, 0.097, 0.198, 0.0478],
+                "E": [0.211, 0.229, 0.254, 0.0984, 0.0757, 0.0446, 0.126, 0.109],
+                "G": [0.66, 0.135, 0.366, 0.0901, 0.565, 0.53, 0.063, 0.412],
+                "Q": [0.0847, 0.0725, 0.0595, 0.0325, 0.0727, 0.0129, 0.0429, 0.0],
+                "K": [0.542, 0.406, 0.117, 0.105, 0.0225, 0.293, 0.533, 0.359],
+                "T": [0.212, 0.162, 0.276, 0.11, 0.176, 0.0809, 0.141, 0.00572],
+            };
+
+            var options = {
+                "colors": jseqlogo.colors.aminoacids
+            };
+            sequence_logo(document.getElementById("logo_aa"), 600, 200, data, options);
+
         </script>
     </body>
 </html>
 ```
+
+![Example logos](example.png)
--- js/jseqlogo.js
+++ js/jseqlogo.js
@@ -1,98 +1,133 @@
+var jseqlogo = {
+    "colors": {
+        "nucleotides": {"A": "green", "C": "blue", "G": "orange", "T": "red", "U": "red"},
+        "aminoacids": {
+            "D": "red", "E": "red",
+            "N": "green", "Q": "green", "S": "green", "G": "green", "T": "green", "Y": "green",
+            "R": "blue", "K": "blue", "H": "blue",
+        }
+    }
+};
+
 function sequence_logo(element, width, height, columns, options) {
-	// set canvas size
-	element.width = width;
-	element.height = height;
+    function range(n) {
+        var a = new Array(n);
+        for(var i = 0; i < a.length; i++) a[i] = i;
+        return a;
+    }
 
-	// Get setttings from options or default values.
-	var defaults = {
-		"colors": {"A": "green", "C": "blue", "G": "orange", "T": "red", "U": "red"},
-		"ylabel": "bits",
-		"textcolor": "black",
-		"bgcolor": "white",
-		"border": 28,
-		"padding": 6,
-		"labelfont": "14px Arial,sans-serif",
-		"letterfont": "30px Arial,sans-serif",
-		"fontpixelheight": 24,
-		"ymax": 0,
-		"sort": true
-	};
+    function sort_order(x) {
+        var order = range(x.length);
+        order.sort(function(a, b) { return x[a] - x[b]; });
+        return order;
+    }
 
-	var settings = {};
-	for(var p in defaults) {
-		settings[p] = (options[p] == null) ? defaults[p] : options[p];
-	}
+    // set canvas size
+    element.width = width;
+    element.height = height;
 
-	// collect stats on columns
-	var ymax = settings.ymax;
-	for (var col = 0; col < columns.length; col++) {
-		var totalweight = 0.0;
-		var column = columns[col];
-		for(var i = 0; i < column.length; ++i) {
-			totalweight += column[i][1];
-		}
-		if (totalweight > ymax) ymax = totalweight;
-	}
-	var ctx = element.getContext("2d");
-	ctx.save();
+    // Get setttings from options or default values.
+    var defaults = {
+        "colors": jseqlogo.colors.nucleotides,
+        "ylabel": "bits",
+        "textcolor": "black",
+        "bgcolor": "white",
+        "border": 28,
+        "padding": 6,
+        "labelfont": "14px Arial,sans-serif",
+        "letterfont": "30px Lucida Console,monospace",
+        "fontpixelheight": 24,
+        "ymax": 0,
+        "sort": true
+    };
 
-	// draw the background
-	ctx.fillStyle = settings.bgcolor;
-	ctx.fillRect(0, 0, width, height);
+    var settings = {};
+    for(var p in defaults) {
+        settings[p] = (options[p] == null) ? defaults[p] : options[p];
+    }
 
-	// draw the letters
-	var columnx = settings.border;
-	var columndelta = (width - settings.border) / columns.length;
-	var yheight = height - settings.border;
-	ctx.font = settings.letterfont;
+    var letters = Object.keys(data);
+    var ncols = data[letters[0]].length;
 
-	for (var col = 0; col < columns.length; col++) {
-		var totalweight = 0.0;
-		var column = columns[col].slice();
-		if(settings.sort) {
-			column.sort(function(a, b) { return a[1] - b[1]; });
-		}
-		var lettery = yheight;
-		for (var i = 0; i < column.length; i++) {
-			var letter = column[i][0];
-			var weight = column[i][1];
-			totalweight += weight;
-			ctx.save();
-			ctx.fillStyle = settings.colors[letter];
-			ctx.translate(columnx, lettery);
-			var scaley = (yheight * weight) / (settings.fontpixelheight * ymax);
-			var mt = ctx.measureText(letter);
-			var letterwidth = mt.width;
-			var scalex = columndelta / letterwidth;
-			ctx.scale(scalex, scaley);
-			ctx.fillText(letter, 0, 0);
-			ctx.restore();
-			lettery -= (weight * yheight) / ymax;
-		}
-		ctx.save();
-		ctx.fillStyle = settings.textcolor;
-		ctx.textAlign = "center";
-		ctx.textBaseline = "top";
-		ctx.font = settings.labelfont;
-		ctx.fillText((col + 1).toString(), columnx + columndelta/2, height-settings.border+settings.padding);
-		ctx.restore();
-		columnx += columndelta;
-	}
+    // collect stats on columns
+    var ymax = settings.ymax;
+    for(var i = 0; i < ncols; i++) {
+        var colsum = 0;
+        for(var j = 0; j < letters.length; j++) {
+            colsum += data[letters[j]][i];
+        }
+        if(colsum > ymax) ymax = colsum;
+    }
 
-	// y-axis labels
-	ctx.fillStyle = settings.textcolor;
-	ctx.font = settings.labelfont;
-	ctx.textAlign = "right";
-	ctx.textBaseline = "top";
-	ctx.fillText(ymax.toPrecision(1), settings.border-settings.padding, 0);
-	ctx.textBaseline = "bottom";
-	ctx.fillText("0", settings.border-settings.padding, height-settings.border);
-	ctx.translate(settings.border-settings.padding, (height-settings.border)/2);
-	ctx.rotate(-Math.PI/2);
-	ctx.textAlign = "center";
-	ctx.textBaseline = "bottom";
-	ctx.fillText(settings.ylabel, 0, 0);
+    var ctx = element.getContext("2d");
+    ctx.save();
 
-	ctx.restore();
-	return element;
+    // draw the background
+    ctx.fillStyle = settings.bgcolor;
+    ctx.fillRect(0, 0, width, height);
+
+    // draw the letters
+    var columnx = settings.border;
+    var columndelta = (width - settings.border) / ncols;
+    var yheight = height - settings.border;
+    ctx.font = settings.letterfont;
+
+    for (var col = 0; col < ncols; col++) {
+        var totalweight = 0.0;
+        var lettery = yheight;
+
+        var values = letters.map(function(letter) { return data[letter][col]; })
+        if(settings.sort) {
+            var order = sort_order(values);
+        } else {
+            var order = range(letters.length);
+        }
+
+        for(var i = 0; i < letters.length; i++) {
+            var letter = letters[order[i]];
+            var weight = data[letter][col];
+
+            if(weight > 0) {
+                totalweight += weight;
+                ctx.save();
+                ctx.fillStyle = letter in settings.colors ? settings.colors[letter] : "black";
+                ctx.translate(columnx, lettery);
+                var scaley = (yheight * weight) / (settings.fontpixelheight * ymax);
+                var mt = ctx.measureText(letter);
+                var letterwidth = mt.width;
+                var scalex = columndelta / letterwidth;
+                ctx.scale(scalex, scaley);
+                ctx.fillText(letter, 0, 0);
+                ctx.restore();
+                lettery -= (weight * yheight) / ymax;
+            }
+        }
+
+        // x-axis labels
+        ctx.save();
+        ctx.fillStyle = settings.textcolor;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "top";
+        ctx.font = settings.labelfont;
+        ctx.fillText((col + 1).toString(), columnx + columndelta/2, height-settings.border+settings.padding);
+        ctx.restore();
+        columnx += columndelta;
+    }
+
+    // y-axis labels
+    ctx.fillStyle = settings.textcolor;
+    ctx.font = settings.labelfont;
+    ctx.textAlign = "right";
+    ctx.textBaseline = "top";
+    ctx.fillText(ymax.toPrecision(1), settings.border-settings.padding, 0);
+    ctx.textBaseline = "bottom";
+    ctx.fillText("0", settings.border-settings.padding, height-settings.border);
+    ctx.translate(settings.border-settings.padding, (height-settings.border)/2);
+    ctx.rotate(-Math.PI/2);
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(settings.ylabel, 0, 0);
+
+    ctx.restore();
+    return element;
 }
